5e6eb655cff9625c05dc5ab4f86f532d1c4419b0
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2020-09-09 23:53:25 +0400

    add method tasks

---
+++ Homeworks/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Method_Task2
+{
+    class Program
+    {
+
+        static void Params(params int[] array)
+        {
+            //Խնդիր 3։ Ստեղծել մեթոդ որին կփոխանցեք զանգված ու այն կտպի բոլոր էլեմենտների արժեքները
+            //Խնդիր 4։ Ստեղծել ֆունկցիա որին կփոխանցեք թվեր ու այն կհաշվի մեծագույնը ու հետ կվերադարձնի այն
+            int maxValue = array[0];
+
+            Console.Write("Array elements: ");
+            for (int i = 0; i < array.Length; i++)
+            {               
+                Console.Write(array[i] + " ");
+                if (array[i] > maxValue)
+                {
+                    maxValue = array[i];
+                }                
+            }
+            Console.Write("\nMax value of array elements: ");
+            Console.WriteLine(maxValue);
+        }
+        static void Main(string[] args)
+        {
+            Params(0, 12, 22, 5, 125, 7, 89);
+            //int[] array = new int[] { 1, 2, 351, 4, 47, 0 };
+            //Params(array);
+            //Params();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homeworks/Is Symmetric Array/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace Two_Dimensional_Array_Task
+{
+    class Program
+    {
+        //Խնդիր 5։ Ստեղծել n x m երկչափ զանգված և նրա էլեմենտները արժևորել կամայական արժեքներով
+
+        static int ParseToNaturalNumber(string stringForPars, string text)
+        {
+            int parsedNumber;
+            while (!int.TryParse(stringForPars, out parsedNumber) || parsedNumber < 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Not a natural number, please try again!");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(text);
+                stringForPars = Console.ReadLine();
+            }
+            return parsedNumber;
+        }
+
+        static int ParseToNumber(string stringForPars, string text)
+        {
+            int parsedNumber;
+            while (!int.TryParse(stringForPars, out parsedNumber))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Not a number, please try again!");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(text);
+                stringForPars = Console.ReadLine();
+            }
+            return parsedNumber;
+        }
+
+        static int ParseToNumberRandomMaxValue(string stringForPars, string text, int minValue)
+        {
+            int parsedNumber;
+            while (!int.TryParse(stringForPars, out parsedNumber) || parsedNumber <= minValue)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Not a number, or max value of random interval less then min value please try again!");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write(text);
+                stringForPars = Console.ReadLine();
+            }
+            return parsedNumber;
+        }
+
+        static int[,] FillTwoDimensionalArrayRandom()
+        {
+            //input array rows length
+            string text1 = "Please input two-dimensional array rows length (natural number greater than 1): r = ";
+            Console.Write(text1);
+            string length1 = Console.ReadLine();
+            int rowLength = ParseToNaturalNumber(length1, text1);
+
+            //input array columns length
+            string text2 = "Please input two-dimensional array columns length (natural number greater than 1): c = ";
+            Console.Write(text2);
+            string length2 = Console.ReadLine();
+            int colLength = ParseToNaturalNumber(length2, text2);
+
+            //input min valur of random interval
+            string text3 = "Please input min value of random interval: a = ";
+            Console.Write(text3);
+            var minValue = Console.ReadLine();
+            int minValueRand = ParseToNumber(minValue, text3);
+
+            //input max valur of random interval
+            string text4 = "Please input max value of random interval: b = ";
+            Console.Write(text4);
+            var maxValue = Console.ReadLine();
+            int maxValueRand = ParseToNumberRandomMaxValue(maxValue, text4, minValueRand);
+
+
+            //fill random two-dimensional array  
+            Random random = new Random();
+            int[,] array = new int[rowLength, colLength];
+            Console.WriteLine("Two-dimensional array elements: ");
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = random.Next(minValueRand, maxValueRand);
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            return array;
+        }
+        static void Main(string[] args)
+        {
+            FillTwoDimensionalArrayRandom();
+            Console.ReadKey();
+        }
+    }
+}
