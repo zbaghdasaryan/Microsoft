df44d38b599147e57e02630af1f65db181a8a037
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2020-11-21 18:21:07 +0400

    Inheritance 2

---
+++ Inheritance_Polymorphism/002_Inheritance/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_2
+{
+    class BaseClass
+    {
+        // Fields
+
+        // public fields.
+        public string publicField = "BaseClass.publicField";
+
+        // protected fields.
+        protected string protectedField = "BaseClass.protectedField";
+
+        // private fields.
+        private string privateField = "BaseClass.privateField";
+
+        public void Show()
+        {
+            Console.WriteLine(privateField);
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        // Constructor.
+        public DerivedClass()
+        {
+            publicField = "DerivedClass.publicField";
+            protectedField = "DerivedClass.protectedField";
+            //privateField = "DerivedClass.privateField";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DerivedClass instance = new DerivedClass();
+
+            Console.WriteLine(instance.publicField);
+
+            instance.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
