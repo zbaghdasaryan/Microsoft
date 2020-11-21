e2cb5ab611ae935ec35b510e25ac5935339a056f
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2020-11-21 18:12:16 +0400

    Inheritance 1

--- Homeworks/ConsoleApp1/Program.cs
+++ Homeworks/ConsoleApp1/Program.cs
@@ -1,35 +1,22 @@
 ﻿using System;
 
-namespace Method_Task2
+namespace factorial_recursion
 {
     class Program
     {
-
-        static void Params(params int[] array)
+        static int factNumber(int number)
         {
-            //Խնդիր 3։ Ստեղծել մեթոդ որին կփոխանցեք զանգված ու այն կտպի բոլոր էլեմենտների արժեքները
-            //Խնդիր 4։ Ստեղծել ֆունկցիա որին կփոխանցեք թվեր ու այն կհաշվի մեծագույնը ու հետ կվերադարձնի այն
-            int maxValue = array[0];
-
-            Console.Write("Array elements: ");
-            for (int i = 0; i < array.Length; i++)
-            {               
-                Console.Write(array[i] + " ");
-                if (array[i] > maxValue)
-                {
-                    maxValue = array[i];
-                }                
+            if (number == 1)
+            {
+                return 1;
             }
-            Console.Write("\nMax value of array elements: ");
-            Console.WriteLine(maxValue);
+            else
+                return number * factNumber(number - 1);
         }
         static void Main(string[] args)
         {
-            Params(0, 12, 22, 5, 125, 7, 89);
-            //int[] array = new int[] { 1, 2, 351, 4, 47, 0 };
-            //Params(array);
-            //Params();
-            Console.ReadLine();
+            Console.WriteLine(factNumber(20));
+            Console.ReadKey();
         }
     }
 }
