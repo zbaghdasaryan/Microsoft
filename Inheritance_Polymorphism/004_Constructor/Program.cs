1ae9e0b1b2b36f93de69dcf3565e715191a20d83
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2020-11-21 18:48:25 +0400

    Constructor 2

--- Inheritance_Polymorphism/004_Constructor/Program.cs
+++ Inheritance_Polymorphism/004_Constructor/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Constuctor_1
+namespace Constuctor_2
 {
     class Program
     {
@@ -28,24 +28,18 @@ namespace Constuctor_1
         {
             public int derivedField;
 
-            // Default constructor.
-            //public DerivedClass() 
-            //{
-            //}
+            //Default constructor.
+            public DerivedClass()
+            {
+            }
 
             /*           
-            When you create an object of a derived class, the constructor of the derived class 
-            automatically calls the default constructor from the base class.
-            The constructor of the base class will assign some of its safe values to all data.
-            After that, the constructor of the derived class will start working, 
-            which will redefine the values for the inherited members. (DOUBLE WORK)!
-             */
+            The custom constructor of the base class is called without having to 
+            assign values to inherited members in the derived class constructor.
+            */
             public DerivedClass(int number1, int number2)
+            : base(number1)
             {
-                // Initialization field of the base class.
-                baseNumber = number1;
-
-                // Initialization field of the derived class.
                 derivedField = number2;
             }
         }
@@ -56,6 +50,8 @@ namespace Constuctor_1
 
             Console.WriteLine(instance.baseNumber);
             Console.WriteLine(instance.derivedField);
+
+            Console.ReadKey();
         }
     }
 }
