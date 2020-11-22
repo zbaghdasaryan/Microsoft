04d63985dbed20b3cdaec206fe6e4b907e38249e
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2020-11-22 13:09:15 +0400

    Polimorphism_NVI

---
+++ Inheritance_Polymorphism/011_Polimorphism/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorphism_NVI
+{
+    class BaseClass 
+    {
+        public virtual void DoWork()
+        {
+            PreDoWork();
+            OtherDoWork();
+        }
+        protected void PreDoWork() { Console.WriteLine("0"); }
+        protected virtual void OtherDoWork() { Console.WriteLine("1"); }
+    }
+    class Derived : BaseClass
+    {
+        protected override void OtherDoWork() {Console.WriteLine("2");}
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass instance = new Derived();
+            instance.DoWork();
+
+            Console.ReadKey();
+        }
+    }
+}
