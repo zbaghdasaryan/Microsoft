1c95d7335c676c0830fe44043797a0562361627c
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2020-12-27 12:24:33 +0400

    Anonymous_001

---
+++ Anonymous/Anonymous_001/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous_001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance = new { Name = "Armen", Age = 25 };
+
+            Console.WriteLine("Name={0}, Age={1}", instance.Name, instance.Age);
+
+            Type type = instance.GetType();
+
+            Console.WriteLine(type.ToString());
+
+            Console.ReadKey();
+        }
+    }
+}
