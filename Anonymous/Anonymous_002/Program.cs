651f916a1ad67f22a357f343db357269f8b05643
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2020-12-27 12:34:26 +0400

    add example 2

---
+++ Anonymous/Anonymous_002/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Anonymous_002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance = new { Name = "Armen", Age = 25, Id = new { Number = 1 } };
+
+            Console.WriteLine("Name={0}, Age={1}, Id={2}", instance.Name, instance.Age, instance.Id.Number);
+
+            Console.ReadKey();
+        }
+    }
+}
