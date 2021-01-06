fc1874f02413d0b9ae78289221f1d99abbac300d
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2021-01-06 15:45:58 +0400

    add Join

---
+++ Threads/Threads_Join/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads_Join
+{
+    class Program
+    {
+        //Method for secondary Thread
+        static void Function()
+        {
+            Console.WriteLine("Secondary Thread ID: {0}", Thread.CurrentThread.GetHashCode());
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(30);
+                Console.Write('.');
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Secondary Thread");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Primary Thread ID: {0}", Thread.CurrentThread.GetHashCode());
+
+            Thread thread = new Thread(new ThreadStart(Function));
+            thread.Start();
+            thread.Join();
+
+            Console.WriteLine("Secondary Thread ID: {0}", Thread.CurrentThread.GetHashCode());
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(30);
+                Console.Write('-');
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Primary Thread");
+
+            Console.ReadKey();
+        }       
+    }
+}
