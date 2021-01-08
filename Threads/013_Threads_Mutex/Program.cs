64f7fa0749e7f35cee0c585f70a6ed0c84162ab8
zorayrbaghdasaryan <zorayrbaghdasaryan@gmail.com>
2021-01-08 17:14:40 +0400

    mutex

---
+++ Threads/013_Threads_Mutex/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threads_Mutex
+{
+    class Program
+    {
+
+        //static Mutex mutex = new Mutex();
+        static Mutex mutex = new Mutex(false, "MyMutex");
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[5];
+            for (int i = 0; i < 5; i++)
+            {
+                threads[i] = new Thread(Function);
+                threads[i].Name = i.ToString();
+                threads[i].Start();
+            }
+            Console.ReadKey();
+        }
+
+        static void Function()
+        {
+            /*
+            Основную работу по синхронизации выполняют методы WaitOne() и ReleaseMutex(). 
+            Метод mutexObj.WaitOne() приостанавливает выполнение потока до тех пор, пока не будет получен мьютекс mutexObj.
+            После выполнения всех действий, когда мьютекс больше не нужен, поток освобождает его с помощью метода mutexObj.ReleaseMutex()
+             */
+
+            mutex.WaitOne();
+            Console.WriteLine("Thread {0} enter safe area", Thread.CurrentThread.Name);
+            Thread.Sleep(2000);
+            Console.WriteLine("Thread {0} exit safe area", Thread.CurrentThread.Name);
+            mutex.ReleaseMutex();
+        }
+    }
+}
